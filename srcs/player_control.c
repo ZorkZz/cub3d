abc217d001e5687d5c0e9d3fcdee8f0a7af3d885
zorkz <alexandre.stachnik2@gmail.com>
2023-09-21 17:27:43 +0200

    fix moov

--- srcs/player_control.c
+++ srcs/player_control.c
@@ -4,7 +4,7 @@ void	move_player(t_game *game, char c)
 {
 	if (c == 'w')
 	{
-		if (game->map.map[(int)(game->perso.x  + cos(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y + sin(game->perso.angle) * MOVE_SPEED) / 100] != '1')
+		if (game->map.map[(int)(game->perso.x  + cos(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y + sin(game->perso.angle) * MOVE_SPEED) / 100] != '1' && game->map.map[(int)(game->perso.x  + cos(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y + sin(game->perso.angle) * MOVE_SPEED) / 100] != '\0')
 		{
 			game->perso.x += cos(game->perso.angle) * MOVE_SPEED;
 			game->perso.y += sin(game->perso.angle) * MOVE_SPEED;
@@ -12,7 +12,7 @@ void	move_player(t_game *game, char c)
 	}
 	else if (c == 's')
 	{
-		if ((game->map.map[(int)(game->perso.x  - cos(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y - sin(game->perso.angle) * MOVE_SPEED) / 100] != '1'))
+		if ((game->map.map[(int)(game->perso.x  - cos(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y - sin(game->perso.angle) * MOVE_SPEED) / 100] != '1') && game->map.map[(int)(game->perso.x - cos(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y - sin(game->perso.angle) * MOVE_SPEED) / 100] != '\0')
 		{
 			game->perso.x -= cos(game->perso.angle) * MOVE_SPEED;
 			game->perso.y -= sin(game->perso.angle) * MOVE_SPEED;
@@ -20,7 +20,7 @@ void	move_player(t_game *game, char c)
 	}
 	else if (c == 'a')
 	{
-		if (game->map.map[(int)(game->perso.x - sin(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y + cos(game->perso.angle) * MOVE_SPEED) / 100] != '1')
+		if (game->map.map[(int)(game->perso.x - sin(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y + cos(game->perso.angle) * MOVE_SPEED) / 100] != '1' && (game->map.map[(int)(game->perso.x - sin(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y + cos(game->perso.angle) * MOVE_SPEED) / 100] != '\0'))
 		{
 			game->perso.x -= sin(game->perso.angle) * MOVE_SPEED;
 			game->perso.y += cos(game->perso.angle) * MOVE_SPEED;
@@ -28,7 +28,7 @@ void	move_player(t_game *game, char c)
 	}
 	else if (c == 'd')
 	{
-		if (game->map.map[(int)(game->perso.x + sin(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y - cos(game->perso.angle) * MOVE_SPEED) / 100] != '1')
+		if (game->map.map[(int)(game->perso.x + sin(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y - cos(game->perso.angle) * MOVE_SPEED) / 100] != '1' && game->map.map[(int)(game->perso.x + sin(game->perso.angle) * MOVE_SPEED) / 100][(int)(game->perso.y - cos(game->perso.angle) * MOVE_SPEED) / 100] != '\0')
 		{
 			game->perso.x += sin(game->perso.angle) * MOVE_SPEED;
 			game->perso.y -= cos(game->perso.angle) * MOVE_SPEED;
