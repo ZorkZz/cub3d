1c8dc440fbc90a51cd443eaabd6c63794d5f0e32
Alexandre Stachnik <astachni@z3r12p1.42lyon.fr>
2023-11-06 16:47:43 +0100

    clc fdp

--- srcs/player_control.c
+++ srcs/player_control.c
@@ -6,7 +6,7 @@
 /*   By: astachni <astachni@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 10:41:19 by astachni          #+#    #+#             */
-/*   Updated: 2023/11/06 10:46:41 by astachni         ###   ########.fr       */
+/*   Updated: 2023/11/06 16:45:12 by astachni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,20 @@ void	move_player(t_game *game, char c)
 	left_right(game, c);
 }
 
+static int	can_moov(char **map, float x, float y)
+{
+	if (map[(int)x][(int)y] != '1' && map[(int)x][(int)y] != '\0')
+		return (1);
+	return (0);
+}
+
 static void	left_right(t_game *game, char c)
 {
 	if (c == 'a')
 	{
-		if (game->map.map[(int)(game->perso.x - sin(game->perso.angle)
-				* MOVE_SPEED)][(int)(game->perso.y + cos(game->perso.angle)
-				* MOVE_SPEED)] != '1' && (game->map.map[(int)(game->perso.x
-				- sin(game->perso.angle) * MOVE_SPEED)][(int)(game->perso.y
-				+ cos(game->perso.angle) * MOVE_SPEED)] != '\0'))
+		if (can_moov(game->map.map, (int)(game->perso.x - (sin(game->perso.angle)
+				* MOVE_SPEED)), (int)(game->perso.y + (cos(game->perso.angle)
+				* MOVE_SPEED))) == 1)
 		{
 			game->perso.x -= sin(game->perso.angle) * MOVE_SPEED;
 			game->perso.y += cos(game->perso.angle) * MOVE_SPEED;
@@ -37,11 +42,9 @@ static void	left_right(t_game *game, char c)
 	}
 	else if (c == 'd')
 	{
-		if (game->map.map[(int)(game->perso.x + sin(game->perso.angle)
-				* MOVE_SPEED)][(int)(game->perso.y - cos(game->perso.angle)
-				* MOVE_SPEED)] != '1' && game->map.map[(int)(game->perso.x
-				+ sin(game->perso.angle) * MOVE_SPEED)][(int)(game->perso.y
-				- cos(game->perso.angle) * MOVE_SPEED)] != '\0')
+		if (can_moov(game->map.map, game->perso.x + sin(game->perso.angle)
+				* MOVE_SPEED, game->perso.y - cos(game->perso.angle)
+				* MOVE_SPEED) == 1)
 		{
 			game->perso.x += sin(game->perso.angle) * MOVE_SPEED;
 			game->perso.y -= cos(game->perso.angle) * MOVE_SPEED;
@@ -53,11 +56,9 @@ static void	forward_backward(t_game *game, char c)
 {
 	if (c == 'w')
 	{
-		if (game->map.map[(int)(game->perso.x + cos(game->perso.angle)
-				* MOVE_SPEED)][(int)(game->perso.y + sin(game->perso.angle)
-				* MOVE_SPEED)] != '1' && game->map.map[(int)(game->perso.x
-				+ cos(game->perso.angle) * MOVE_SPEED)][(int)(game->perso.y
-				+ sin(game->perso.angle) * MOVE_SPEED)] != '\0')
+		if (can_moov(game->map.map, (int)(game->perso.x + (sin(game->perso.angle)
+				* MOVE_SPEED)), (int)(game->perso.y + (cos(game->perso.angle)
+				* MOVE_SPEED))) == 1)
 		{
 			game->perso.x += cos(game->perso.angle) * MOVE_SPEED;
 			game->perso.y += sin(game->perso.angle) * MOVE_SPEED;
@@ -65,11 +66,9 @@ static void	forward_backward(t_game *game, char c)
 	}
 	else if (c == 's')
 	{
-		if ((game->map.map[(int)(game->perso.x - cos(game->perso.angle)
-					* MOVE_SPEED)][(int)(game->perso.y - sin(game->perso.angle)
-			* MOVE_SPEED)] != '1') && game->map.map[(int)(game->perso.x
-				- cos(game->perso.angle) * MOVE_SPEED)][(int)(game->perso.y
-				- sin(game->perso.angle) * MOVE_SPEED)] != '\0')
+		if (can_moov(game->map.map, game->perso.x - sin(game->perso.angle)
+				* MOVE_SPEED, game->perso.y - cos(game->perso.angle)
+				* MOVE_SPEED) == 1)
 		{
 			game->perso.x -= cos(game->perso.angle) * MOVE_SPEED;
 			game->perso.y -= sin(game->perso.angle) * MOVE_SPEED;
